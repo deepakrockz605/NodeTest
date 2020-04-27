c8e8d57ee4fccb8eb4e7bc4f676730b8f837bf85
Your Name <deepakpawar618@gmail.com>
2020-04-27 18:56:24 +0530

    refactored our codes

--- app.js
+++ app.js
@@ -1,15 +1,12 @@
-// Call in installed dependencies
-const express = require("express");
-// set up express app
+const express = require('express');
+
 const app = express();
-// set up port number
+
 const port = 5035;
-// set up home route
-app.get("/", (request, respond) => {
-  respond.status(200).json({
-    message: "Welcome to Project Support",
+
+app.get('/', (req, res) => {
+  res.status(200).json({
+    message: 'Welcome to Project Support 2',
   });
 });
-app.listen(port, (request, respond) => {
-  console.log(`Our server is live on ${port}. Yay!`);
-});
+app.listen(port)
